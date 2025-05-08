0d801252b9ecb17fe7fc8a62ac22ba79c10391cc
hassanmohamedd <hassanmouhamedd>
2025-05-08 04:35:27 +0300

    ..

--- controlusers/users.js
+++ controlusers/users.js
@@ -4,76 +4,73 @@ const users = [
       id: 1,
       name: "Alice Johnson",
       email: "alice@example.com",
-      isActive: true
+      isActive: true,
     },
     {
       id: 2,
       name: "Bob Smith",
       email: "bob@example.com",
-      isActive: false
+      isActive: false,
     },
     {
       id: 3,
       name: "Charlie Davis",
       email: "charlie@example.com",
-      isActive: true
+      isActive: true,
     },
     {
       id: 4,
       name: "Diana Evans",
       email: "diana@example.com",
-      isActive: true
+      isActive: true,
     },
     {
       id: 5,
       name: "Ethan Brown",
       email: "ethan@example.com",
-      isActive: false
-    }
+      isActive: false,
+    },
   ];
   
-
-const getUsers =async (req,res) => {
-   res.status(200).json({
-    status:200,
-            data: {data :users,message:"users fetched success" },
-        })
-
+  const getUsers = async (req, res) => {
+    res.status(200).json({
+      status: 200,
+      data: { data: users, message: "Users fetched successfully" },
+    });
+  
     console.log("get users");
-};
-
-
-
-const addUsers =async (req,res) => {
-
-    console.log("add users ");
-};
-
-const deleteUsers =async (req,res) => {
-
-    console.log("Delete Users ");
-};
-const getsingleUsers =async(req,res)=>{
-    const { userId} = req.params;
-    const singleUser =Users.find((user) => user.id== userId);
-    
-    if(!singleUser){
-
-        return res.status(404).json({
-            status:404,
-            data : {data :null,message:"invalid user ID" },
-        })
+  };
+  
+  const addUsers = async (req, res) => {
+    console.log("add users");
+  };
+  
+  const deleteUsers = async (req, res) => {
+    console.log("delete users");
+  };
+  
+  const getsingleUsers = async (req, res) => {
+    const { userId } = req.query;
+  
+    const singleUser = users.find((user) => user.id == userId);
+  
+    if (!singleUser) {
+      return res.status(404).json({
+        status: 404,
+        data: { data: null, message: "Invalid user ID" },
+      });
     }
+  
     return res.status(200).json({
-        status:200,
-        data : {data :singleUser,message:"invalid user ID" },
-    })
-
-    
-
-};
-
-
-module. exports = {
-    getUsers,addUsers,deleteUsers,getsingleUsers,
-};
+      status: 200,
+      data: { data: singleUser, message: "User fetched successfully" },
+    });
+  };
+  
+  module.exports = {
+    getUsers,
+    addUsers,
+    deleteUsers,
+    getsingleUsers,
+  };
+  
