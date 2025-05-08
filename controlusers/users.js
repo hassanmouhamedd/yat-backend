3fa3cbec19eb945794f3647087d868f905b5d23e
hassanmohamedd <hassanmouhamedd>
2025-05-08 04:30:58 +0300

    .

--- controlusers/users.js
+++ controlusers/users.js
@@ -1,4 +1,38 @@
 //req=requaried , res= responted, 3lashan el front ye4ofh
+const users = [
+    {
+      id: 1,
+      name: "Alice Johnson",
+      email: "alice@example.com",
+      isActive: true
+    },
+    {
+      id: 2,
+      name: "Bob Smith",
+      email: "bob@example.com",
+      isActive: false
+    },
+    {
+      id: 3,
+      name: "Charlie Davis",
+      email: "charlie@example.com",
+      isActive: true
+    },
+    {
+      id: 4,
+      name: "Diana Evans",
+      email: "diana@example.com",
+      isActive: true
+    },
+    {
+      id: 5,
+      name: "Ethan Brown",
+      email: "ethan@example.com",
+      isActive: false
+    }
+  ];
+  
+
 const getUsers =async (req,res) => {
    res.status(200).json({
     status:200,
